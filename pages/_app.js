1038d68a7cb9d59bfc36cc80fc36dc99c959546f
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2025-08-06 21:02:43 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import '../styles/globals.css';
 import Layout from '../comps/Layout';
-import { Provider } from 'react-redux';
-import store from '../redux/store'; // Adjust the path if necessary
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </Provider>
+    
   );
 }
 
