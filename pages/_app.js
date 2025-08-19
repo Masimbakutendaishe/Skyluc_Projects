e6f20e0f8a964837b47862b2a761c8f283de666f
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2025-08-19 22:09:59 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
 import Layout from '../comps/Layout';
-
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    
+    <>
+      <Head>
+        <link rel="icon" href="/skyluclogo.png" />
+        <title>Skyluc Projects & Logistics</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    
+    </>
   );
 }
 
